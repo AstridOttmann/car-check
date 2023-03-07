4fa5af44e34aeda811bd17c6bee9253fe7dadd3a
Astrid Ottmann <astrid.ottmann@gmail.com>
2023-03-07 17:44:47 +0100

    initial commit

---
+++ src/main/java/org/example/Car.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public class Car {
+    private int numberOfTires;
+    private int numberOfDoors;
+    private boolean seatBelt;
+    private boolean airbag;
+public Car(){};
+public Car(int numberOfTires, int numberOfDoors, boolean seatBelt, boolean airbag){
+    this.numberOfTires = numberOfTires;
+    this.numberOfDoors = numberOfDoors;
+    this.seatBelt = seatBelt;
+    this.airbag = airbag;
+}
+
+}
